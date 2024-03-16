7d299da24971c778d976e5d30ab1dc1a25da4bd0
Javier Martinez <fjmb.mx@gmail.com>
2024-03-15 18:35:49 -0600

    Primer version de paginas homepage para goldenluxury, terraprime, telemetry y onelovehuatulco

---
+++ goldenluxury/js/send-email.js
@@ -0,0 +1,64 @@
+
+$(document).ready(function () {
+
+    $('#form_saved_data_contact').on('submit', function (event) {
+        if (!event.target.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+
+        }
+        $(this).addClass('was-validated')
+    });
+
+    // -----------------------------Guardar datos de contacto---------------------------
+
+    $("#form_saved_data_contact").on('submit', function (event) {
+
+
+        var prospect_name = $("#prospect_name").val();
+        var prospect_contact = $("#prospect_contact").val();
+        var prospect_subject = $("#prospect_subject").val();
+        var prospect_message = $("#prospect_message").val();
+
+        if (prospect_name == '' || prospect_contact == '' || prospect_subject == '' || prospect_message == '') {
+            return false;
+        }
+
+        $.ajax({
+            contentType: "application/json; charset=UTF-8",
+            data: JSON.stringify({
+                "prospect_name": prospect_name, "prospect_contact": prospect_contact,
+                "prospect_subject": prospect_subject, "prospect_message": prospect_message
+            }),
+            type: "POST",
+            dataType: 'json',
+            url: "core/email/enviocorreo.php",
+
+            success: function (response) {
+
+                if (response.hasOwnProperty('status')) {
+                    if (response.status == '200') {
+                        $('#form_saved_data_contact').each(function () {
+                            this.reset();
+                        });
+                        $('#form_saved_data_contact')
+                            .trigger("reset")
+                            .removeClass('was-validated')
+                        swal("!  Muy bien ! ", "En breve nos pondremos en contacto con usted !", "success");
+
+                    } else {
+                        swal("Intentelo más tarde!", "No se ha podido enviar el mensaje", "warning")
+
+                    }
+                }
+
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                swal("Intentelo más tarde!", "", "warning")
+
+            }
+        });
+
+        return true;
+    }); //---------------------------------------Fin guardar datos contacto------------------------------------
+});
---
+++ telemetry/README.md
@@ -0,0 +1,59 @@
+# [ - ] Home Page Telemetry  [2022]
+ 
+Pagina principal de real 
+
+<br/>
+
+![Application](img/main.PNG?raw=true)
+
+## Comenzando 🚀
+
+---
+
+Estas instrucciones te permitirán obtener una copia del proyecto en funcionamiento en tu máquina local para propósitos
+de desarrollo y pruebas.
+
+1.- Clona el repositorio.
+
+| Comando | Descripción |
+| --- | --- |
+| HTTP | `https://github.com/fjmbmx/homepages.git` |
+| SSH | `git@github.com:fjmbmx/homepages.git` |
+
+
+2.- Importa el proyecto a algún IDE como un maven project.
+ 
+
+### Pre-requisitos 📋
+
+---
+
+| Nombre | Version |Url |
+|   :---:   |   :---:   |   :---:   |
+|   HTML    |   5   | [] |
+|   PHP    |  8.0.0 >   | [https://www.php.net/](https://www.php.net/)  
+
+### Instalación 🔧
+
+.....
+
+## Despliegue 📦
+
+---
+
+ 
+
+## Versionado 📌
+
+---
+
+ ....
+ 
+
+## Autor✒️
+
+---
+ 
+* **MARTINEZ BAUTISTA, FRANCISCO JAVIER** - *Autor* - [Telegram](https://t.me/+KhXmGSrV3JU4M2Yx)  
+ 
+ 
